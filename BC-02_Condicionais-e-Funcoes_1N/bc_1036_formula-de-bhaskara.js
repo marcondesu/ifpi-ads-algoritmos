4f86467b8fa2c68314d8b8aa4c3cf17509268991
Marcos Vinícius <rkatchau@gmail.com>
2022-04-30 15:13:01 -0300

    Consertando bc_1036

--- BC-02_Condicionais-e-Funcoes_1N/bc_1036_formula-de-bhaskara.js
+++ BC-02_Condicionais-e-Funcoes_1N/bc_1036_formula-de-bhaskara.js
@@ -23,8 +23,8 @@ function calcularRaizesEquacao2Grau(a, b, c) {
    if (a === 0 || ((b**2 - 4*a*c) < 0)) {
        return false
    } else {
-       const raiz_1 = ((-b + Math.sqrt(b*b - 4*a*c)) / 2*a) / 100
-       const raiz_2 = ((-b - Math.sqrt(b*b - 4*a*c)) / 2*a) / 100
+       const raiz_1 = ((-b + Math.sqrt(b*b - 4*a*c)) / (2*a))
+       const raiz_2 = ((-b - Math.sqrt(b*b - 4*a*c)) / (2*a))
 
        return [raiz_1.toFixed(5), raiz_2.toFixed(5)]
    }
