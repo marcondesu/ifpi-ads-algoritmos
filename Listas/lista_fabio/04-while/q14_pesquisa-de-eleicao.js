bdbc501e9a7638b97e7844d6d3b805e9e520357a
Marcos Vinícius <rkatchau@gmail.com>
2022-06-25 16:31:29 -0300

    add q14

---
+++ Listas/lista_fabio/04-while/q14_pesquisa-de-eleicao.js
@@ -0,0 +1,34 @@
+import { loadFile } from '../../../Iteracao-strings/input-utils.js'
+
+function main() {
+    console.clear()
+    console.log('------- Resultado de pesquisa de eleição -------\n')
+
+    const lines = loadFile('../../../Testes/lista04while_q14.txt').split('\r\n').map(Number)
+
+    let counter = 0
+    let [serra, dilma, ciro] = [0, 0, 0]
+    let [indeciso, outros, nulo] = [0, 0, 0]
+
+    while (lines[counter] !== -1) {
+        if (lines[counter] === 13) {dilma++}
+        else if (lines[counter] === 23) {ciro++}
+        else if (lines[counter] === 45) {serra++}
+        else if (lines[counter] === 99) {indeciso++}
+        else if (lines[counter] === 98) {outros++}
+        else if (lines[counter] === 0) {nulo++}
+
+        counter++
+    }
+
+    serra = ((serra/ counter) * 100).toFixed(2)
+    ciro = ((ciro/ counter) * 100).toFixed(2)
+    dilma = ((dilma/ counter) * 100).toFixed(2)
+    outros = ((outros/ counter) * 100).toFixed(2)
+    nulo = ((nulo/ counter) * 100).toFixed(2)
+
+    console.log(`Porcentagens:\n\tSerra: ${serra}%\n\tCiro: ${ciro}%\n\tDilma: ${dilma}%`)
+    console.log(`\tOutros: ${outros}%\n\tNulo: ${nulo}%\n\nTotal de entrevistados: ${counter}`)
+}
+
+main()
