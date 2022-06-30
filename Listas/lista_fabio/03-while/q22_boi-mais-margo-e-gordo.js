7ab6f631630623ad8e3f6b8186fda4302d71f95f
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2022-06-30 11:52:27 -0300

    add q22

---
+++ Listas/lista_fabio/03-while/q22_boi-mais-margo-e-gordo.js
@@ -0,0 +1,32 @@
+import { loadFile } from '../../../Iteracao-strings/input-utils.js'
+
+function main() {
+    const list = loadFile('../../../Testes/lista03_q22.txt').split('\n')
+    
+    console.clear()
+    console.log('\t Determinar boi mais magro e boi mais gordo \t\n')
+
+    let [magro, gordo] = [[``, Number.MAX_VALUE], [``, 0]]
+    let [nome, peso] = [``, 0]
+
+    let counter = 0
+    
+    while (counter < list.length) {
+        [nome, peso] = list[counter].split(' ')
+
+        if (peso > gordo[1]) {
+            gordo = [`${nome}`, Number(peso)]
+        }
+
+        if (peso < magro[1]) {
+            magro = [`${nome}`, Number(peso)]
+        }
+
+        counter++
+    }
+
+    console.log(`O boi mais magro é o ${magro[0]} pesando ${magro[1].toFixed(2)}kg`)
+    console.log(`O boi mais gordo é o ${gordo[0]} pesando ${gordo[1].toFixed(2)}kg`)
+}
+
+main()
