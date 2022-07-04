d9e0e41d606c69df5d2ddc1ef0ce2f05f219e2f0
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2022-07-04 12:56:21 -0300

    update io-utils

--- Utils/io-utils.js
+++ Utils/io-utils.js
@@ -1,6 +1,18 @@
 import prompt from 'prompt-sync'
 const input = prompt()
 
+export function getText(msg) {
+    const text = input(msg)
+
+    if (!isNaN(text)) {
+        console.log('Erro! O valor deve ser um texto.\n')
+
+        return getText(msg)
+    }
+
+    return text
+}
+
 export function getNumber(msg) {
     const number = input(msg)
 
@@ -73,14 +85,6 @@ export function getNumberSmallerThan(max, msg) {
     return number
 }
 
-export function getText(msg) {
-    const text = input(msg)
-
-    if (!isNaN(text)) {
-        console.log('Erro! O valor deve ser um texto.\n')
-
-        return getText(msg)
-    }
-
-    return text
+export function randomValue(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
 }
